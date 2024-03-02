7d885e016d8d6e2e395f8d5e5d74476b830c7a87
books4thoughtgit <avelinaenriquez1956@gmail.com>
2024-03-02 11:56:36 +0800

    Continuing on my laptop

--- src/mindustrialist/content/blocks/PowerBlocks.java
+++ src/mindustrialist/content/blocks/PowerBlocks.java
@@ -5,3 +5,26 @@ import mindustry.world.*;
 import mindustry.world.draw.*;
 import mindustrialist.world.blocks.power.*;
 import mindustrialist.world.draw.*;
+
+import static mindustry.type.ItemStack.*;
+
+public class PowerBlocks{
+    public static Block
+            SolarPanelI,
+            LVPole,
+            WindTurbine,
+            LVBattery,
+            MVPole,
+            MVBattery,
+            DoubleMVBattery,
+    
+    end;
+    
+    public static void load(){
+        SolarPanelI = new SolarGenerator("solar-panel1"){{
+            requirements(Category.power, with(Item.money, 10));
+            
+            
+        }};
+    }
+}
